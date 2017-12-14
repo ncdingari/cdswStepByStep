9f3afed13b17d005c763f708ddae329ef0ed1293
Shravan Pabba <spabba@cloudera.com>
2017-12-14 00:18:44 +0000

    modified 150 Step

--- mkparquettable.sh
+++ mkparquettable.sh
@@ -1,4 +1,5 @@
-impala-shell -i HOSTRUNNINGIMPALAD:21000 <<eoj
+impala-shell -i 10.0.0.41:21000 <<eoj
+drop table sample_07p;
 create table sample_07p stored as parquet as select * from sample_07;
-select * from dropme limit 5;
+select * from sample_07p limit 5;
 eoj
